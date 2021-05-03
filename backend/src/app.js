89abd1a9ae195c5a1084b1a4947beda0063e7c3e
nathielems <nathielems@users.noreply.github.com>
2021-05-02 23:35:20 -0300

    add cors and body-parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(cors())
 
 const routes = require('./routes');
 const basePath = '/v1';
+app.use(bodyParser.json({ limit: '200kb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 routes.init(app, basePath);
 
